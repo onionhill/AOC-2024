6e3a3a656d52fa61b08afc4e655c5cd06e254bfb
Vegard Løkås <vegard.lokas@bouvet.no>
2024-12-20 10:50:10 +0100

    part 1

---
+++ python/20/1.py
@@ -0,0 +1,55 @@
+import time
+start_time = time.time()
+
+
+def read_file(filename):
+    with open(filename) as file:
+        grid = file.readlines()
+    return grid
+
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1) ]
+
+grid = read_file("input.txt")
+start = ()
+end = ()
+for y, line in enumerate(grid):
+    for x, char in enumerate(line):
+        if char == 'S':
+            start = (y,x)
+        if char == 'E':
+            end = (y,x)
+
+
+track = {start:0}
+cur = start
+curstep = 0
+while cur != end:
+    curstep += 1
+    y,x = cur
+    for dy,dx in DIRECTIONS:
+        new_y = y + dy
+        new_x = x + dx
+        if (new_y,new_x) not in track and grid[new_y][new_x] in 'SE.':
+            cur = (new_y,new_x)
+            track[cur] = curstep
+            break
+
+count = 0
+for y,x in track:
+    for dy,dx in DIRECTIONS:
+        current_pos = (y, x)
+        next_pos = (y + dy, x + dx)
+        next_next_pos = (y + 2 * dy, x + 2 * dx)
+
+        if next_pos not in track and next_next_pos in track and track[next_next_pos] - track[current_pos] >= 101:
+            count += 1
+            print(next_pos, track[next_next_pos] - track[current_pos] )
+print(count)
+
+
+
+
+
+end_time = time.time()
+elapsed_time_ms = (end_time - start_time) * 1000
+print(f"Elapsed time: {elapsed_time_ms} milliseconds")
